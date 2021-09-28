7a93b380f763d4512fda6a8f73ed554652c9296f
Celian <celian.opigez@edu.esiee-it.fr>
2021-09-28 10:00:05 +0200

    Changements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+
+        System.out.println("Entrez votre prénom");
+        String Scanner = "";
+        java.util.Scanner sc;
+        sc = new Scanner(System.in);
+        String i = sc.nextLine();
+
+
+        System.out.println("Salut " + i);
+
+
 
     }
 }
